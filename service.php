6411598af2bb91b48fbc167326bf4a030044fd1b
kumahacker <kumahavana@gmail.com>
2020-01-19 12:14:56 -0500

    core3

--- service.php
+++ service.php
@@ -1,9 +1,10 @@
 <?php
 
-use Goutte\Client;
+use Framework\Crawler;
 use Apretaste\Request;
 use Apretaste\Response;
 use Apretaste\Challenges;
+use Framework\Utils;
 
 /**
  * Granma Service
@@ -19,9 +20,9 @@ class Service
 	 */
 	public function _main(Request $request, Response &$response)
 	{
-	    $response->setCache("day");
+	    $response->setCache('day');
 		$response->setLayout('granma.ejs');
-		$response->setTemplate("allStories.ejs", $this->allStories());
+		$response->setTemplate('allStories.ejs', $this->allStories());
 	}
 
 	/**
@@ -35,14 +36,14 @@ class Service
 	public function _buscar(Request $request, Response &$response)
 	{
 		$buscar = $request->input->data->searchQuery;
-		$isCategory = $request->input->data->isCategory == "true";
+		$isCategory = $request->input->data->isCategory =='true';
 
 		// no allow blank entries
 		if(empty($buscar)){
 			$response->setLayout('granma.ejs');
 			$response->setTemplate('text.ejs', [
-				"title" => "Su busqueda parece estar en blanco",
-				"body" => "debe decirnos sobre que tema desea leer"
+					'title' => 'Su busqueda parece estar en blanco',
+					'body'  => 'debe decirnos sobre que tema desea leer'
 			]);
 			return;
 		}
@@ -53,21 +54,21 @@ class Service
 		// error if the searche return empty
 		if(empty($articles)) {
 			$response->setLayout('granma.ejs');
-			$response->setTemplate("text.ejs", [
-				"title" => "Su busqueda parece estar en blanco",
-				"body" => html_entity_decode("Su busqueda no gener&oacute; ning&uacute;n resultado. Por favor cambie los t&eacute;rminos de b&uacute;squeda e intente nuevamente.")
+			$response->setTemplate('text.ejs', [
+					'title' => 'Su busqueda parece estar en blanco',
+					'body'  => html_entity_decode('Su busqueda no gener&oacute; ning&uacute;n resultado. Por favor cambie los t&eacute;rminos de b&uacute;squeda e intente nuevamente.')
 			]);
 			return;
 		}
 
 		$content = [
-			"articles" => $articles,
-			"isCategory" => $isCategory,
-			"search" => $buscar
+				'articles'   => $articles,
+				'isCategory' => $isCategory,
+				'search'     => $buscar
 		];
 
 		$response->setLayout('granma.ejs');
-		$response->setTemplate("searchArticles.ejs", $content);
+		$response->setTemplate('searchArticles.ejs', $content);
 	}
 
 	/**
@@ -92,7 +93,7 @@ class Service
 		// send data to the template
 		$response->setCache('year');
 		$response->setLayout('granma.ejs');
-		$response->setTemplate("story.ejs", $content, $images);
+		$response->setTemplate('story.ejs', $content, $images);
 
 		Challenges::complete('read-granma', $request->person->id);
 	}
@@ -107,44 +108,44 @@ class Service
 	private function search($query)
 	{
 		// get content from cache
-		$cache = TEMP_PATH . "granma_" . md5($query) . date("Ymd") . ".cache";
+		$cache = TEMP_PATH .'granma_'. md5($query) . date('Ymd') .'.cache';
 		if(file_exists($cache)) $articles = unserialize(file_get_contents($cache));
 
 		// crawl the data from the web
 		else {
-			// Setup crawler
-			$client = new Client();
-			$crawler = $client->request('GET',"http://www.granma.cu/archivo?q=" . urlencode($query));
+			Crawler::start('http://www.granma.cu/archivo?q='. urlencode($query));
 
 			// Collect search by term
 			$articles = [];
 
-			$crawler->filter('div.col-md-12.g-searchpage-results article.g-searchpage-story')->each(function ($item) use (&$articles) {
+			Crawler::filter('div.col-md-12.g-searchpage-results article.g-searchpage-story')->each(function ($item) use (&$articles) {
 				// only allow news, no media or gallery
+
+				/** @var \Symfony\Component\DomCrawler\Crawler $item */
 				if ($item->filter('.ico')->count() > 0) return;
 
 				// get data from each row
 				$title = $item->filter('h2 a')->text();
 				$info = $item->filter('p.g-story-meta')->text();
-				$info = explode("de",$info);
+				$info = explode('de',$info);
 				$day = trim($info[0]);
 				$month = trim($info[1]);
-				$info = explode("@",$info[2]);
+				$info = explode('@',$info[2]);
 				$year = trim($info[0]);
-				$info = explode("|",$info[1]);
+				$info = explode('|',$info[1]);
 				$hour = trim($info[0]);
 				$author = trim($info[1]);
 				$info = "$month $day, $year. $hour &bull; <i>$author</i>";
 
 				$description = $item->filter('p')->text();
-				$link = $item->filter('a')->attr("href");
+				$link = $item->filter('a')->attr('href');
 
 				// store list of articles
 				$articles[] = [
-					"pubDate" => $info,
-					"description" => $description,
-					"title" => $title,
-					"link" => $link
+						'pubDate'     => $info,
+						'description' => $description,
+						'title'       => $title,
+						'link'        => $link
 				];
 			});
 
@@ -164,18 +165,18 @@ class Service
 	private function allStories()
 	{
 		// get content from cache
-		$cache = TEMP_PATH . "granma_" . date("Ymd") . ".cache";
+		$cache = TEMP_PATH .'granma_'. date('Ymd') .'.cache';
 		if(file_exists($cache)) $articles = unserialize(file_get_contents($cache));
 
 		// crawl the data from the web
 		else {
 			// create a crawler
-			$page = file_get_contents("http://www.granma.cu/feed");
+			$page = Crawler::get('http://www.granma.cu/feed');
 
 			$content = simplexml_load_string($page, null, LIBXML_NOCDATA);
 
 			$articles = [];
-			if (!isset($content->channel)) return ["articles" => []];
+			if (!isset($content->channel)) return ['articles' => []];
 
 			foreach ($content->channel->item as $item) {
 				// get all parameters
@@ -184,8 +185,8 @@ class Service
 				$description = strip_tags($item->description);
 				setlocale(LC_ALL, 'es_ES.UTF-8');
 				$pubDate = $item->pubDate;
-				$pubDate = strftime("%B %d, %Y.",strtotime($pubDate))." ".date_format((new DateTime($pubDate)),'h:i a');
-				$dc = $item->children("http://purl.org/dc/elements/1.1/");
+				$pubDate = strftime('%B %d, %Y.',strtotime($pubDate)).' '.date_format((new DateTime($pubDate)),'h:i a');
+				$dc = $item->children('http://purl.org/dc/elements/1.1/');
 				$author = $dc->creator;
 
 				// get all the categories
@@ -197,13 +198,13 @@ class Service
 
 				// get the article
 				$articles[] = [
-					"title" => (String)$title,
-					"link" => (String)$link,
-					"pubDate" => (String)$pubDate,
-					"description" => (String)$description,
-					"category" => $category,
-					"categoryLink" => [],
-					"author" => (String)$author
+						'title'        => (String)$title,
+						'link'         => (String)$link,
+						'pubDate'      => (String)$pubDate,
+						'description'  => (String)$description,
+						'category'     => $category,
+						'categoryLink' => [],
+						'author'       => (String)$author
 				];
 			}
 
@@ -212,7 +213,7 @@ class Service
 		}
 
 		// return response content
-		return ["articles" => $articles];
+		return ['articles' => $articles];
 	}
 
 	/**
@@ -226,39 +227,38 @@ class Service
 	private function story($query)
 	{
 		// get content from cache
-		$cache = TEMP_PATH . "granma_" . md5($query) . ".cache";
+		$cache = TEMP_PATH .'granma_'. md5($query) .'.cache';
 		if(file_exists($cache)) $story = unserialize(file_get_contents($cache));
 
 		// crawl the data from the web
 		else {
-			$client = new Client();
-			$crawler = $client->request('GET',"http://www.granma.cu/$query");
+			Crawler::start("http://www.granma.cu/$query");
 
 			// search for title
-			$title = $crawler->filter('div.g-story-meta h1.g-story-heading')->text();
+			$title = Crawler::filter('div.g-story-meta h1.g-story-heading')->text();
 
 			// get the intro
 
-			$titleObj = $crawler->filter('div.g-story-meta p.g-story-description');
-			$intro = $titleObj->count() > 0 ? $titleObj->text() : "";
+			$titleObj = Crawler::filter('div.g-story-meta p.g-story-description');
+			$intro = $titleObj->count() > 0 ? $titleObj->text() :'';
 
 			// get the images
-			$imageObj = $crawler->filter('div.image img');
-			$imgAlt = "";
-			$img = "";
-			if ($imageObj->count() != 0) {
-				$imgUrl = trim($imageObj->attr("src"));
-				$imgAlt = trim($imageObj->attr("alt"));
+			$imageObj = Crawler::filter('div.image img');
+			$imgAlt = '';
+			$img = '';
+			if ($imageObj->count() !== 0) {
+				$imgUrl = trim($imageObj->attr('src'));
+				$imgAlt = trim($imageObj->attr('alt'));
 
 				// get the image
 				if (!empty($imgUrl)) {
-					$img = TEMP_PATH . Utils::generateRandomHash() . "." . pathinfo($imgUrl, PATHINFO_EXTENSION);
-					file_put_contents($img, file_get_contents("http://www.granma.cu$imgUrl"));
+					$img = TEMP_PATH . Utils::randomHash() .'.'. pathinfo($imgUrl, PATHINFO_EXTENSION);
+					file_put_contents($img, Crawler::get("http://www.granma.cu$imgUrl"));
 				}
 			}
 
 			// get the array of paragraphs of the body
-			$paragraphs = $crawler->filter('div.story-body-textt p');
+			$paragraphs = Crawler::filter('div.story-body-textt p');
 			$content = [];
 			foreach ($paragraphs as $p) {
 				$content[] = trim($p->textContent);
@@ -266,12 +266,12 @@ class Service
 
 			// create a json object to send to the template
 			$story = [
-				"title" => $title,
-				"intro" => $intro,
-				"img" => $img,
-				"imgAlt" => $imgAlt,
-				"content" => $content,
-				"url" => "http://www.granma.cu/$query"
+					'title'   => $title,
+					'intro'   => $intro,
+					'img'     => $img,
+					'imgAlt'  => $imgAlt,
+					'content' => $content,
+					'url'     => "http://www.granma.cu/$query"
 			];
 
 			// create the cache
@@ -286,14 +286,11 @@ class Service
 	 *
 	 * @param String
 	 * @return String
-	 * http://www.martinoticias.com/content/blah
 	 */
-	private function urlSplit($url)
-	{
-		$url = explode("/", trim($url));
-		unset($url[0]);
-		unset($url[1]);
-		unset($url[2]);
-		return implode("/", $url);
+	private function urlSplit($url): string {
+		$url = explode('/', trim($url));
+		unset($url[0], $url[1], $url[2]);
+
+		return implode('/', $url);
 	}
 }
